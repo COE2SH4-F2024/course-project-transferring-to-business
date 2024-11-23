895aee799300140380af5833644b91b903f1c951
laeeqm <laeeqm@mcmaster.ca>
2024-11-23 15:56:12 -0500

    Iteration 0 complete

--- Project.cpp
+++ Project.cpp
@@ -15,7 +15,10 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
-
+char input;
+objPos arb1(4, 4, 'w');
+objPos arb2(2, 4, 'q');
+objPos arb3(7, 8, 'a');
 
 int main(void)
 {
@@ -45,12 +48,22 @@ void Initialize(void)
 
 void GetInput(void)
 {
-   
+   if (MacUILib_hasChar()){
+        input = MacUILib_getChar();
+    }
 }
 
 void RunLogic(void)
 {
-    
+    if(input != 0)  // if not null character
+    {
+        switch(input)
+        {                      
+            case 27:  // char 27 is the escape key
+                exitFlag = 1;
+                break;
+        }
+    }
 }
 
 void DrawScreen(void)
@@ -63,6 +76,15 @@ void DrawScreen(void)
         for (j=0; j <= 19; j++){
             if (i == 0 || i == 9 || j == 0 || j == 19) {
                 MacUILib_printf("#");
+            }
+            else if (i == arb1.pos->x && j == arb1.pos->y) {
+                MacUILib_printf("%c", arb1.symbol);
+            }
+            else if (i == arb2.pos->x && j == arb2.pos->y) {
+                MacUILib_printf("%c", arb2.symbol);
+            }
+            else if (i == arb3.pos->x && j == arb3.pos->y) {
+                MacUILib_printf("%c", arb3.symbol);
             }
             else {
                 MacUILib_printf(" ");
